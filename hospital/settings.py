8d103bccab166c93859ba82e667bf230fb7f8908
KUNJESH YDV <rajanshu23187@gmail.com>
2023-11-15 22:01:42 +0530

    Update settings.py

--- hospital/settings.py
+++ hospital/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gor(dk_z!v7x!!2lcp@eyeut#w6rq2h%s!$cssz4g-foax*@x#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [""]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
